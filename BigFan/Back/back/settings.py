674374759b1174020c4ea764e0eb11818dde8595
Luke <gunhee9177@naver.com>
2020-12-25 01:30:50 +0900

    mysql 적용

--- BigFan/Back/back/settings.py
+++ BigFan/Back/back/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -95,12 +94,9 @@ WSGI_APPLICATION = 'back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import my_settings
+
+DATABASES = my_settings.DATABASES
 
 
 # Password validation
---
+++ BigFan/Back/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bigfan',                 
+        'USER': 'root',                      
+        'PASSWORD': 'rjsdn@1luck',              
+        'HOST': 'localhost',                 
+        'PORT': '3306',                      
+    }
+}
